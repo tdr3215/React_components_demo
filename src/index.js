f7183e548449762850fc006c4c955ed866e6482d
Tiffany Rose <tiffany.drose@gmail.com>
2021-09-20 05:31:01 -0400

    reusable props

--- src/index.js
+++ src/index.js
@@ -3,28 +3,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45 PM"
-        img={faker.image.avatar()}
-        commentText="Awesome Job!"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00 AM"
-        img={faker.image.avatar()}
-        commentText="Cool Post!"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00 PM"
-        img={faker.image.avatar()}
-        commentText="What?!"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          img={faker.image.avatar()}
+          commentText="Awesome Job!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00 AM"
+          img={faker.image.avatar()}
+          commentText="Cool Post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 PM"
+          img={faker.image.avatar()}
+          commentText="What?!"
+        />
+      </ApprovalCard>
     </div>
   );
 };
